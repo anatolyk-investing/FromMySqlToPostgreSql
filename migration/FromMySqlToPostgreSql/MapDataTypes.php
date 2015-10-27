7e79fdf31c769adca782771c508e2a033c9b7b7b
anatolyk-investing <anatoly.k@investing.com>
2015-10-27 17:25:58 +0200

    Update MapDataTypes.php

--- migration/FromMySqlToPostgreSql/MapDataTypes.php
+++ migration/FromMySqlToPostgreSql/MapDataTypes.php
@@ -15,7 +15,7 @@
  * 
  * You should have received a copy of the GNU General Public License
  * along with this program (please see the "LICENSE.md" file).  
- * If not, see <http://www.gnu.org/licenses/gpl.txt>.
+ * If not, see <http://www.gnu.org/licenses/gpl.txt>
  */
 
 /**
